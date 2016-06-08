b4e03e4b964c13335304c3fee5c15c1b12ca1663
Brandon Manson <brandon.manson@detroitlabs.com>
2016-06-08 11:29:15 -0400

    initial commit with XCode project

---
+++ HereForBeer/HereForBeer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HereForBeer
+//
+//  Created by Brandon Manson on 6/8/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
