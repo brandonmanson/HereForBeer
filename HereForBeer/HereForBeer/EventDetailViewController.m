ba802c6b1532393e5698b97371336c331b0e1192
msmi1023 <msmi1023@ford.com>
2016-06-08 15:03:44 -0400

    Initial storyboard work and controller skeletons

---
+++ HereForBeer/HereForBeer/EventDetailViewController.m
@@ -0,0 +1,45 @@
+//
+//  EventDetailViewController.m
+//  HereForBeer
+//
+//  Created by tstone10 on 6/8/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "EventDetailViewController.h"
+
+@interface EventDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UILabel *eventNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventStartTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventEndTimeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventVenueNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *eventVenueStreetAddressLabel;
+
+@end
+
+@implementation EventDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ HereForBeer/HereForBeer/EventTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventTableViewController.h
+//  HereForBeer
+//
+//  Created by tstone10 on 6/8/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventTableViewController : UITableViewController
+
+@end
